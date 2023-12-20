2aa0e1a5ae942e324e0dc086527980ec6cba4fa3
mariaelisaaraya <arayamariaelisa@gmail.com>
2023-12-20 17:20:45 -0300

    12,13,14

--- challenge12/index.js
+++ challenge12/index.js
@@ -1,25 +1,25 @@
 function checkIsValidCopy(original, copy) {
-    if (original.length !== copy.length) {
-      return false;
-    }
-  
-    const charRx = /([a-z#])/i;
-    const copyRx = /([.:+#\s])/;
-  
-    for (let i = 0; i < original.length; i++) {
-      const origChr = original.charAt(i);
-      const copyChr = copy.charAt(i);
-  
-      if (origChr !== copyChr && origChr.toLowerCase() !== copyChr) {
-        if (charRx.test(origChr)) {
-          if (!copyRx.test(copyChr)) {
-            return false;
-          }
-        } else {
+  if (original.length !== copy.length) {
+    return false;
+  }
+
+  const charRx = /([a-z#])/i;
+  const copyRx = /([.:+#\s])/;
+
+  for (let i = 0; i < original.length; i++) {
+    const origChr = original.charAt(i);
+    const copyChr = copy.charAt(i);
+
+    if (origChr !== copyChr && origChr.toLowerCase() !== copyChr) {
+      if (charRx.test(origChr)) {
+        if (!copyRx.test(copyChr)) {
           return false;
         }
+      } else {
+        return false;
       }
     }
-  
-    return true;
   }
+
+  return true;
+}
--- challenge14/index.js
+++ challenge14/index.js
@@ -1,14 +1,14 @@
 function maxGifts(houses) {
-    const n = houses.length;
-  
-    if (n < 2) return n === 1 ? houses[0] : 0;
-  
-    const [first, second, ...rest] = houses;
-    const dp = [first, Math.max(first, second)];
-  
-    for (let i = 2; i < n; i++) {
-      dp[i] = Math.max(dp[i - 1], dp[i - 2] + houses[i]);
-    }
-  
-    return dp[n - 1];
+  const n = houses.length;
+
+  if (n < 2) return n === 1 ? houses[0] : 0;
+
+  const [first, second, ...rest] = houses;
+  const dp = [first, Math.max(first, second)];
+
+  for (let i = 2; i < n; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + houses[i]);
   }
+
+  return dp[n - 1];
+}
