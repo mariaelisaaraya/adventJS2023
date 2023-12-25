de359c5e83cb65bd47863f8d2a07947c6edfa393
mariaelisaaraya <arayamariaelisa@gmail.com>
2023-12-25 17:40:36 -0300

    mejorando codigo

--- challenge02/index.js
+++ challenge02/index.js
@@ -1,6 +1,9 @@
-function manufacture(gifts, materials) {
-    return gifts.filter(gift => {
-        const uniqueChars = [...new Set(gift + materials)];
-        return uniqueChars.length === materials.length;
-    });
-}
+function findNaughtyStep(original, modified) {
+    if (original === modified) return "";
+  
+    let index = 0;
+    while (original[index] === modified[index++]);
+  
+    return (original.length > modified.length ? original : modified)[index - 1];
+  }
+  
--- challenge19/index.js
+++ challenge19/index.js
@@ -1,19 +1,30 @@
 function revealSabotage(store) {
-    const n = store.length;
-    const m = store[0].length;
-  
-    for (let i = 0; i < n; ++i) {
-      for (let j = 0; j < m; ++j) {
-        const ij = store[i][j];
-        const w = +(store[i - 1]?.[j - 1] === '*') + +(store[i - 1]?.[j] === '*');
-        const y = +(store[i - 1]?.[j + 1] === '*') + +(store[i]?.[j - 1] === '*');
-        const t = +(store[i]?.[j + 1] === '*') + +(store[i + 1]?.[j - 1] === '*');
-        const k = +(store[i + 1]?.[j] === '*') + +(store[i + 1]?.[j + 1] === '*');
-        const x = `${w + y + t + k}`;
-        store[i][j] = [ij, x][+(w + y + t + k > 0) * +(ij === ' ')];
+  let currentRow = null;
+  let nextRow;
+
+  for (const [i, row] of store.entries()) {
+    nextRow = store[i + 1];
+
+    for (const [b, cell] of row.entries()) {
+      if (cell !== "*") {
+        const calc =
+          +(currentRow?.[b - 1] === "*") +
+          +(currentRow?.[b] === "*") +
+          +(currentRow?.[b + 1] === "*") +
+          +(row?.[b - 1] === "*") +
+          +(row?.[b + 1] === "*") +
+          +(nextRow?.[b - 1] === "*") +
+          +(nextRow?.[b] === "*") +
+          +(nextRow?.[b + 1] === "*");
+
+        if (calc > 0) {
+          row[b] = calc.toString();
+        }
       }
     }
-  
-    return store;
+    currentRow = row;
   }
+
+  return store;
+}
   
